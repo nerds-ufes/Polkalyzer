d73059165e6d557ef64ac1f5e1039552e535c182
lucas <lucas@propi.dev>
2023-08-26 12:53:47 -0300

    bug fix

--- scripts/restoreEnv.sh
+++ scripts/restoreEnv.sh
@@ -3,11 +3,11 @@
 # Answer are you sure? if yes, then print "Restoring environment..." with case sensitive
 read "Are you sure you want to restore the environment? (y/n)" -p answer
 case $answer in
-	[yY]* )
+	y|Y)
 		echo "Restoring environment..."
 		git restore .
 		git clean -fd
 	;;
-	[nN]* ) exit;;
-	* ) echo "Invalid Answer, aborting ...";;
+	n|N) echo "Aborting ..." ;;
+	* )echo "Invalid Answer, aborting ..." ;;
 esac
