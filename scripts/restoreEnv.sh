c4e67f973d6b9b8cc63901ba28022525872ce576
lucas <lucas@propi.dev>
2023-08-26 12:47:34 -0300

    bug fix

--- scripts/restoreEnv.sh
+++ scripts/restoreEnv.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Answer are you sure? if yes, then restore the environment
-echo "Are you sure you want to restore the environment? (y/n)" -p Answer
-if [ "$Answer" != "${Answer#[Yy]}" ] ;then
-	echo "Restoring environment..."
-	git restore .
-	git clean -fd .
+# Answer are you sure? if yes, then print "Restoring environment..." with case sensitive
+read "Are you sure you want to restore the environment? (y/n)" -p answer
+case $answer in
+	[yY]* )
+		echo "Restoring environment..."
+		git restore .
+		git clean -fd
+	;;
+	[nN]* ) exit;;
+	* ) echo "Invalid Answer, aborting ...";;
+esac
