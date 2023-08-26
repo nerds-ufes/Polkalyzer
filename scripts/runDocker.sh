d24d9aa59b70352d1f1cd8209978acb653653fa1
lucas <lucas@propi.dev>
2023-08-26 13:35:09 -0300

    update

--- scripts/runDocker.sh
+++ scripts/runDocker.sh
@@ -3,5 +3,5 @@ echo "Starting docker container ..."
 docker start polkalyzer || sudo docker start polkalyzer
 echo "Running terminal on docker container ..."
 docker exec -it polkalyzer /bin/bash || sudo docker exec -it polkalyzer /bin/bash
-echo "Stoping docker container, please wait a while ..."
+echo "Stopping docker container, please wait a while ..."
 docker stop polkalyzer || sudo docker stop polkalyzer
